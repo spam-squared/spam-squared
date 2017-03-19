5936cd5bc58f5bc6612673e923c92e507bac929e
LeanderK <Leander.Kurscheidt@gmx.de>
2017-03-19 11:17:12 +0100

    added keyword extractor

---
+++ tensorflow/keyword.py
@@ -0,0 +1,15 @@
+import requests
+import json
+
+class Keyword(object):
+
+    url = "http://api.meaningcloud.com/topics-2.0"
+
+    def getKeywords(self, text):
+        payload = "key=ca2aff042ede68ee24c7225387167015&lang=eng&txt="+text+"&tt=a"
+        headers = {'content-type': 'application/x-www-form-urlencoded'}
+
+        response = requests.request("POST", self.url, data=payload, headers=headers)
+
+        data = json.loads(response)
+        filter(lambda x: x['relevance'] >= 50,data['entity_list'])
