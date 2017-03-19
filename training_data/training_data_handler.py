408be9de3b7039c7de1a8786fe5e49a1f96fbde0
LeanderK <Leander.Kurscheidt@gmx.de>
2017-03-19 13:02:51 +0100

    fixed db bug Version 2

--- training_data/training_data_handler.py
+++ training_data/training_data_handler.py
@@ -2,10 +2,11 @@ import sqlite3
 
 
 class TrainingDataHandler(object):
+    conn = sqlite3.connect('training_data.db')
+
     def init_db(self):
         """Init database"""
-        conn = sqlite3.connect('training_data.db')
-        c = conn.cursor()
+        c = self.conn.cursor()
         c.execute('''
           CREATE TABLE IF NOT EXISTS train_data (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -16,7 +17,7 @@ class TrainingDataHandler(object):
           );
         ''')
 
-        conn.commit()
+        self.conn.commit()
         c.close()
 
     def load_db(self):
@@ -28,7 +29,6 @@ class TrainingDataHandler(object):
         data = list(c)
 
         conn.commit()
-        conn.close()
         return data
 
     def load_without_target(self):
@@ -39,7 +39,6 @@ class TrainingDataHandler(object):
         data = list(c)
 
         conn.commit()
-        conn.close()
         return data
 
     def set_target(self, spammer, target):
@@ -58,30 +57,29 @@ class TrainingDataHandler(object):
         """)
 
         conn.commit()
-        conn.close()
 
     def insert_sample(self, data, target, spammer):
         """Insert a training sample into database, target -1 if not yet present"""
-        conn = sqlite3.connect('training_data.db')
 
-        c = conn.cursor()
+
+        c = self.conn.cursor()
         c.execute(f"""
           UPDATE train_data
            SET answered = 1
           WHERE spammer = '{spammer}'
           """)
 
-        conn.commit()
-        conn.close()
+        self.conn.commit()
+        c.close()
 
-        c = conn.cursor()
+        c = self.conn.cursor()
         c.execute("""
           INSERT INTO train_data (target, answered, data, spammer)
-          VALUES ('%s',  '%s', '%s');
-        """ % target, "FALSE", data, spammer)
+          VALUES ('%s',  '%s', '%s', '%s');
+        """ % (target, "FALSE", data, spammer))
 
-        conn.commit()
-        conn.close()
+        self.conn.commit()
+        c.close()
 
     def get_training_data(self):
         """Get training data"""
