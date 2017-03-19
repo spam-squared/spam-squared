46c21c86e0e4a7acc82951f35210ddac937638c6
LeanderK <Leander.Kurscheidt@gmx.de>
2017-03-19 10:43:50 +0100

    integrated tensorflow into main

--- main.py
+++ main.py
@@ -1,16 +1,30 @@
 from mail_service.mail_service import MailService
 from training_data.training_data_handler import TrainingDataHandler
 from decision_tree.decision_tree import DecisionTree
+from tensorflow.model_v2 import Tensorflow
+import argparse
+
+parser = argparse.ArgumentParser(description='Process some integers.')
+parser.add_argument('--tensorflow', dest='tensorflow', action='store_true')
+parser.add_argument('--no-tensorflow', dest='tensorflow', action='store_false')
+parser.set_defaults(tensorflow=False)
+
+args = parser.parse_args()
 
 # Set up database
 training_data_handler = TrainingDataHandler()
 training_data_handler.init_db()
 training_data_handler.load_db()
 
-# Set up decision tree
-decision_tree = DecisionTree()
-decision_tree.train_tree(training_data_handler.get_training_data())
-decision_tree.test()
+if (not args.tensorflow):
+    # Set up decision tree
+    decision_tree = DecisionTree()
+    decision_tree.train_tree(training_data_handler.get_training_data())
+    decision_tree.test()
+else:
+    tensorflow = Tensorflow()
+    restored = tensorflow.restore()
+
 
 # Set up email service
 mailService = MailService()
--- tensorflow/model_v2.py
+++ tensorflow/model_v2.py
@@ -27,100 +27,95 @@ from sklearn import metrics
 import tensorflow as tf
 from tensorflow.contrib.layers.python.layers import encoders
 
-learn = tf.contrib.learn
 
-FLAGS = None
+class Tensorflow(object):
+    learn = tf.contrib.learn
 
-MAX_DOCUMENT_LENGTH = 10
-EMBEDDING_SIZE = 50
-n_words = 0
-batch_size = 15
+    FLAGS = None
 
+    MAX_DOCUMENT_LENGTH = 10
+    EMBEDDING_SIZE = 50
+    n_words = 0
+    batch_size = 15
 
-def rnn_model(features, target):
-    # Create a LSTM Unit cell with hidden size of EMBEDDING_SIZE.
-    cell = tf.nn_cell.BasicLSTMCell(650)
+    def rnn_model(self, features, target):
+        # Create a LSTM Unit cell with hidden size of EMBEDDING_SIZE.
+        cell = tf.nn_cell.BasicLSTMCell(650)
 
-    # Create an unrolled Recurrent Neural Networks to length of
-    # MAX_DOCUMENT_LENGTH and passes word_list as inputs for each unit.
-    _, encoding = tf.contrib.rnn.static_rnn(cell, tf.constant(features.head(batch_size), name='not_saved'), dtype=tf.float32)
+        # Create an unrolled Recurrent Neural Networks to length of
+        # MAX_DOCUMENT_LENGTH and passes word_list as inputs for each unit.
+        _, encoding = tf.contrib.rnn.static_rnn(cell, tf.constant(features.head(self.batch_size), name='not_saved'),
+                                                dtype=tf.float32)
 
-    # Given encoding of RNN, take encoding of last step (e.g hidden size of the
-    # neural network of last step) and pass it as features for logistic
-    # regression over output classes.
-    target = tf.one_hot(target, batch_size, 1, 0)
-    logits = tf.contrib.layers.fully_connected(encoding, 15, activation_fn=None)
-    loss = tf.contrib.losses.softmax_cross_entropy(logits, target)
+        # Given encoding of RNN, take encoding of last step (e.g hidden size of the
+        # neural network of last step) and pass it as features for logistic
+        # regression over output classes.
+        target = tf.one_hot(target, self.batch_size, 1, 0)
+        logits = tf.contrib.layers.fully_connected(encoding, 15, activation_fn=None)
+        loss = tf.contrib.losses.softmax_cross_entropy(logits, target)
 
-    # Create a training op.
-    train_op = tf.contrib.layers.optimize_loss(
-        loss,
-        tf.contrib.framework.get_global_step(),
-        optimizer='Adam',
-        learning_rate=0.01)
+        # Create a training op.
+        train_op = tf.contrib.layers.optimize_loss(
+            loss,
+            tf.contrib.framework.get_global_step(),
+            optimizer='Adam',
+            learning_rate=0.01)
 
-    return ({
-                'class': tf.argmax(logits, 1),
-                'prob': tf.nn.softmax(logits)
-            }, loss, train_op)
+        return ({
+                    'class': tf.argmax(logits, 1),
+                    'prob': tf.nn.softmax(logits)
+                }, loss, train_op)
 
+    # train, test = [{'keywords':[],‘id':1,'responded':True}]
+    def train_nn(self, train, test):
+        x_train = map(lambda data: self.transform(data), train)
+        y_train = map(lambda data: data['responded'], train)
 
-# train, test = [{'keywords':[],‘id':1,'responded':True}]
-def train_nn(train, test):
+        x_test = map(lambda data: self.transform(data), test)
+        y_test = map(lambda data: self.data['responded'], test)
 
-    x_train = map(lambda data: transform(data), train)
-    y_train = map(lambda data: data['responded'], train)
+        classifier = self.learn.Estimator(model_fn=self.rnn_model)
 
-    x_test = map(lambda data: transform(data), test)
-    y_test = map(lambda data: data['responded'], test)
+        # Train and predict
+        classifier.fit(x_train, y_train, steps=100)
+        y_predicted = [
+            p['class'] for p in classifier.predict(
+                x_test, as_iterable=True)
+            ]
+        score = metrics.accuracy_score(y_test, y_predicted)
+        print('Accuracy: {0:f}'.format(score))
 
-    classifier = learn.Estimator(model_fn=rnn_model)
+    def classify(self, keywords, id):
+        classifier = self.learn.Estimator(model_fn=self.rnn_model)
+        input = np.array(keywords[:].insert(0, id))
 
-    # Train and predict
-    classifier.fit(x_train, y_train, steps=100)
-    y_predicted = [
-        p['class'] for p in classifier.predict(
-            x_test, as_iterable=True)
-        ]
-    score = metrics.accuracy_score(y_test, y_predicted)
-    print('Accuracy: {0:f}'.format(score))
+        return classifier.predict(input)
 
+    def transform(self, data):
+        transformed = map(lambda elem: hashlib.md5("elem".encode('utf-8')).hexdigest(), data['keywords']).insert(0,
+                                                                                                                 data[
+                                                                                                                     'id'])
+        return transformed
 
-def classify(keywords, id):
-    classifier = learn.Estimator(model_fn=rnn_model)
-    input = np.array(keywords[:].insert(0, id))
+    def save(self, checkpoint_file='hello.chk'):
+        with tf.Session() as session:
+            x = tf.Variable([42.0, 42.1, 42.3], name='x')
+            y = tf.Variable([[1.0, 2.0], [3.0, 4.0]], name='y')
+            not_saved = tf.Variable([-1, -2], name='not_saved')
+            session.run(tf.initialize_all_variables())
 
-    return classifier.predict(input)
+            print(session.run(tf.all_variables()))
+            saver = tf.train.Saver([x, y])
+            saver.save(session, checkpoint_file)
 
+    def restore(self, checkpoint_file='hello.chk'):
+        restored = False
+        with tf.Session() as session:
+            saver = tf.train.Saver()
+            saver.restore(session, checkpoint_file)
+            print(session.run(tf.all_variables()))
+            restored = True
+        return restored
 
-def transform(data):
-    transformed = map(lambda elem: hashlib.md5("elem".encode('utf-8')).hexdigest(), data['keywords']).insert(0, data['id'])
-    return transformed
-
-
-def save(checkpoint_file='hello.chk'):
-    with tf.Session() as session:
-        x = tf.Variable([42.0, 42.1, 42.3], name='x')
-        y = tf.Variable([[1.0, 2.0], [3.0, 4.0]], name='y')
-        not_saved = tf.Variable([-1, -2], name='not_saved')
-        session.run(tf.initialize_all_variables())
-
-        print(session.run(tf.all_variables()))
-        saver = tf.train.Saver([x, y])
-        saver.save(session, checkpoint_file)
-
-
-def restore(checkpoint_file='hello.chk'):
-    x = tf.Variable(-1.0, validate_shape=False, name='x')
-    y = tf.Variable(-1.0, validate_shape=False, name='y')
-    with tf.Session() as session:
-        saver = tf.train.Saver()
-        saver.restore(session, checkpoint_file)
-        print(session.run(tf.all_variables()))
-
-
-def reset():
-    tf.reset_default_graph()
-
-
-
+    def reset(self):
+        tf.reset_default_graph()
